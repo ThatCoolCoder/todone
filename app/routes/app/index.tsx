0c8a5c9eb77ed7475b04dbf5e6cf72ea36f47a06
ThatCoolCoder <ThatCoolCoder@users.noreply.github.com>
2025-03-07 13:00:33 +1030

    start making filters for main page

--- app/routes/app/index.tsx
+++ app/routes/app/index.tsx
@@ -1,10 +1,11 @@
-import { PlusIcon } from "@heroicons/react/20/solid";
-import { Button, Group } from "@mantine/core";
+import { ChevronDownIcon, ChevronUpIcon, PlusIcon } from "@heroicons/react/20/solid";
+import { Button, Card, Collapse, Fieldset, Grid, Group, OptionsDropdown, Select, Text } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import { modals } from '@mantine/modals';
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import AddTodoPopup from "~/components/Todo/AddTodoPopup";
-import TodoList from "~/components/Todo/TodoList";
+import TodoCard from "~/components/Todo/TodoCard";
 import { TodosContext } from "~/context/TodosContext";
 import { Todo } from "~/data/Todo";
 import db from "~/services/database";
@@ -27,7 +28,7 @@ export default function Index() {
             size: "lg",
             children: (
                 <>
-                {/* Modal is rendered outside of context so just give it to him */}
+                {/* Modal is rendered in a portal (ie outside of context) so just give the data to him */}
                     <AddTodoPopup allTodos={allTodos} />
                 </>
             )
@@ -40,9 +41,56 @@ export default function Index() {
                 <p>Todos</p>
                 <Button onClick={addTodo} variant="outline" color="white"><PlusIcon className="size-6" /></Button>
             </Group>
-            <div>
-                <TodoList hideDone={false}></TodoList>
-            </div>
+            <br />
+            <TodoList hideDone={false}></TodoList>
         </TodosContext.Provider>
     </>
+}
+
+function TodoList({hideDone}: {hideDone: boolean}) {
+    const allTodos = useContext(TodosContext);
+
+    const displayedTodos = hideDone ? allTodos.val.filter(t => ! t.done) : allTodos.val;
+
+    return<>
+        <TodoListFilters />
+        <br />
+        <Grid>
+            {allTodos.val.map(todo => 
+                <Grid.Col key={todo.id}><TodoCard todo={todo} /></Grid.Col>
+            )}
+        </Grid>
+        <br />
+        {displayedTodos.length == 0 && <Text fz="large">No todos yet</Text>}
+    </>
+}
+
+
+enum DoneFilteringOptions {
+    All,
+    Done,
+    NotDone
+}
+
+const names = {
+    [DoneFilteringOptions.All] : "All",
+    [DoneFilteringOptions.Done] : "Done",
+    [DoneFilteringOptions.NotDone] : "NotDone",
+};
+
+function TodoListFilters() {
+    const [opened, { toggle }] = useDisclosure(false);
+
+    const [doneFiltering, setDoneFiltering] = useState(DoneFilteringOptions.NotDone);
+
+    return <>
+        <Fieldset p="0">
+            <Group className="bg-neutral-700 px-4 cursor-pointer" onClick={toggle}>Filtering {opened ? <ChevronUpIcon className="size-6" /> : <ChevronDownIcon className="size-6" />}</Group>
+
+            <Collapse in={opened}>
+                <div className="p-4">
+                filter yes hehe
+            </Collapse>
+        </Fieldset>
+    </>;
 }
